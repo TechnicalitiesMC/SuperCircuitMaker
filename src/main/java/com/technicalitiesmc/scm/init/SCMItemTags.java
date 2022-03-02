0e7b252053687b5c676ddc104846abf9a6ffed01
amadornes <amadornes@gmail.com>
2022-03-02 16:23:09 +0100

    Update to MC 1.18.2 and bump version to 0.2.2

--- src/main/java/com/technicalitiesmc/scm/init/SCMItemTags.java
+++ src/main/java/com/technicalitiesmc/scm/init/SCMItemTags.java
@@ -1,13 +1,14 @@
 package com.technicalitiesmc.scm.init;
 
 import com.technicalitiesmc.scm.SuperCircuitMaker;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.ItemTags;
-import net.minecraft.tags.Tag;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 
 public class SCMItemTags {
 
-    public static final Tag.Named<Item> SHOWS_CIRCUIT_GRID = ItemTags.bind(SuperCircuitMaker.MODID + ":shows_circuit_grid");
-    public static final Tag.Named<Item> ROTATES_COMPONENTS = ItemTags.bind(SuperCircuitMaker.MODID + ":rotates_components");
+    public static final TagKey<Item> SHOWS_CIRCUIT_GRID = ItemTags.create(new ResourceLocation(SuperCircuitMaker.MODID + ":shows_circuit_grid"));
+    public static final TagKey<Item> ROTATES_COMPONENTS = ItemTags.create(new ResourceLocation(SuperCircuitMaker.MODID + ":rotates_components"));
 
 }
